a4850e53a93e9f1a073c5a69a958d881afa25996
Rup medhi <rupmmedhi@gmail.com>
2023-03-30 16:08:34 +0530

    task2 Add Restaurant Summary

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Layout/Header';
+import FoodSummary from "./Components/Meals/FoodSummary";
 
 function App() {
   return (
     <>
       <Header />
+      <FoodSummary />
     </>
   );
 }
